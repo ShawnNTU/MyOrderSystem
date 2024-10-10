49fb9b5a2273d0292fbc7c217ee4d73539b39cf7
SU JYUN SYUN <shawn900905@gmail.com>
2024-10-11 00:26:14 +0800

    feat: Update settings.py
    remove an unused app

--- SuMingXingSite/SuMingXingSite/settings.py
+++ SuMingXingSite/SuMingXingSite/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'printer',
     'database.apps.DatabaseConfig',
     "corsheaders", # CORS !
 ]
