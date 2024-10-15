ce940628d4e1a7ef0a12598f16325f076fb73a05
OceanView PC <shawn900905@gmail.com>
2024-10-15 21:48:07 +0800

    fix : fix z index of
    fix the z index problem in "DropdownMenu.jsx"

--- SuMingXingSite/database/DatabaseFrontend/src/Component/DropdownMenu.jsx
+++ SuMingXingSite/database/DatabaseFrontend/src/Component/DropdownMenu.jsx
@@ -14,28 +14,28 @@ export function DropdownMenu({display_value, snippet, handleRowClickEvent}){
                 {open && 
                     <>
                         {/* this component is for closing the dropmenu when click "outside the dropmenu" */}
-                        <div className="fixed left-0 top-0 w-screen h-screen"
+                        <div className="fixed left-0 top-0 z-5 w-screen h-screen"
                             onClick={()=>{open && setOpen(false)}}
                         ></div>
                         {/* dropdown menu */}
                         <div className={"absolute left-0 top-8 z-10 max-w-60 max-h-60 overflow-y-scroll overflow-x-hidden bg-white border border-stone-300 shadow-lg"} 
                             onBlur={()=>{ open?setOpen(false):setOpen(true)}}
                         >
-                        {/* search bar */}
-                        <input type="text" className="sticky top-0 shadow-lg border border-neutral-300"
-                            placeholder="輸入名稱可以篩選"
-                            value={search_input}
-                            onChange={(e)=>{setSearchInput(e.target.value)}}
-                        />
-                        {/* rows of search result */}
-                        {snippet.map(x =>{
-                            if (x.includes(search_input)){
-                                return (
-                                    <div key={x} className="border-b border-stone-300 p-1 text-left hover:bg-neutral-300 hover:cursor-pointer" 
-                                        onClick={()=>{handleRowClickEvent(x)}}>{x}</div>
-                                )
-                            }
-                        })}
+                            {/* search bar */}
+                            <input type="text" className="sticky top-0 shadow-lg border border-neutral-300"
+                                placeholder="輸入名稱可以篩選"
+                                value={search_input}
+                                onChange={(e)=>{setSearchInput(e.target.value)}}
+                            />
+                            {/* rows of search result */}
+                            {snippet.map(x =>{
+                                if (x.includes(search_input)){
+                                    return (
+                                        <div key={x} className="border-b border-stone-300 p-1 text-left hover:bg-neutral-300 hover:cursor-pointer" 
+                                            onClick={()=>{handleRowClickEvent(x)}}>{x}</div>
+                                    )
+                                }
+                            })}
                         </div>
                     </>
                 }
